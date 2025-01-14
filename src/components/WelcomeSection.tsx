4689e031593a7f6e18d5fa71469a79dbe679a454
yahya0211 <yahyaagung65@gmail.com>
2025-01-14 15:35:49 +0700

    finished

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
@@ -18,9 +19,9 @@ const WelcomeSection = () => {
           </div>
         </div>
         <div className="welcome-section__items welcome-section__items--image">
-          <img src="/image(1).png" alt="welcome-image" className="welcome-section__items--image1" />
+          <Image src="/image(1).png" alt="welcome-image" className="welcome-section__items--image1" />
           <div className="welcome-section__items--border">
-            <img src="/image(1).png" alt="info-image" className="welcome-section__items--text-green" />
+            <Image src="/image(1).png" alt="info-image" className="welcome-section__items--text-green" />
             <div className="welcome-section__items--subborder">
               <div className="welcome-section__items--bold">Green Salad Tomato</div>
               <div className="welcome-section__items-rating1">Tomato</div>
@@ -35,10 +36,10 @@ const WelcomeSection = () => {
           </div>
         </div>
         <h1 className="welcome-section__items--description2">I would think that conserving our natural resources should be a conservative position: Not to waste food, and not to throw away a lot of the food that we buy.</h1>
-            <div className="welcome-section__items--button2">
-              <button className="navbar__auth--register">Daftar Sekarang</button>
-              <button className="welcome-section__items--button2">About Us</button>
-            </div>
+        <div className="welcome-section__items--button2">
+          <button className="navbar__auth--register">Daftar Sekarang</button>
+          <button className="welcome-section__items--button2">About Us</button>
+        </div>
       </div>
     </>
   );
